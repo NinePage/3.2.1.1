5454322ab73fc623166b66cd99e57b1765183533
NinePage <ninepageart@gmail.com>
2021-07-30 16:06:01 +0400

    Start

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 4000;
+        int bonus = price / 20;
+        System.out.println("Bonus: " + bonus + " miles");
+    }
+}
